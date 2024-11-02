f71a1d66d00a3853f8c611e7b1a860cec6527c3e
Maxim_Letov <mr.letov.maxim@gmail.com>
2024-11-02 23:38:58 +0300

    Fixed the 2nd method of data extraction

--- ECG/Source/Game/Public/PlayerStateBase2.h
+++ ECG/Source/Game/Public/PlayerStateBase2.h
@@ -15,153 +15,30 @@ struct FUntypedEmplaceRegistry;
 
 DECLARE_LOG_CATEGORY_EXTERN(LogPlayerStateBase2, Log, All);
 
-// #pragma region TRAITS
-//
-// 		template <typename T>
-// 		struct TMemberTraits : public TMemberTraits<decltype(&T::operator())>
-// 		{};
-//
-// 		template<typename...>
-// 		struct TArgsPack {};
-//
-// 		
-// 		template <typename R, typename... Args>
-// 		struct TMemberTraits<R(*)(Args...)>
-// 		{
-// 			using ReturnType = R;
-// 			using ArgsTuple = TTuple<Args...>;
-//
-// 			using ArgsPack = TArgsPack<Args...>;
-// 			
-// 			template <std::size_t Index>
-// 			using Argument = std::tuple_element_t<
-// 				Index,
-// 				std::tuple<Args...>
-// 			>;
-// 			
-// 			static constexpr auto Arity = sizeof...(Args);
-// 		};
-// 		
-// 		template <typename ClassType, typename R, typename... Args>
-// 		struct TMemberTraits<R(ClassType::*)(Args...)>
-// 		{
-// 			using ReturnType = R;
-// 			using ArgsTuple = TTuple<Args...>;
-// 			using ArgsPack = TArgsPack<Args...>;
-// 			
-// 			template <std::size_t Index>
-// 			using Argument = std::tuple_element_t<
-// 				Index,
-// 				std::tuple<Args...>
-// 			>;
-//
-// 			static constexpr auto Arity = sizeof...(Args);
-// 		};
-// 		
-// 		template <typename ClassType, typename R, typename... Args>
-// 		struct TMemberTraits<R(ClassType::*)(Args...) const>
-// 		{
-// 			using ReturnType = R;
-// 			using ArgsTuple = TTuple<Args...>;
-// 			using ArgsPack = TArgsPack<Args...>;
-// 			
-// 			template <std::size_t Index>
-// 			using Argument = std::tuple_element_t<
-// 				Index,
-// 				std::tuple<Args...>
-// 			>;
-// 			static constexpr auto Arity = sizeof...(Args);
-// 		};	
-//
-// 		template <typename ClassType, typename R, typename... Args>
-// 		struct TMemberTraits<R(ClassType::*)(Args...) &>
-// 		{
-// 			using ReturnType = R;
-// 			using ArgsTuple = TTuple<Args...>;
-// 			using ArgsPack = TArgsPack<Args...>;
-// 			
-// 			template <std::size_t Index>
-// 			using Argument = std::tuple_element_t<
-// 				Index,
-// 				std::tuple<Args...>
-// 			>;
-//
-// 			static constexpr auto Arity = sizeof...(Args);
-// 		};
-//
-// 		template <typename ClassType, typename R, typename... Args>
-// 		struct TMemberTraits<R(ClassType::*)(Args...) &&>
-// 		{
-// 			using ReturnType = R;
-// 			using ArgsTuple = TTuple<Args...>;
-// 			using ArgsPack = TArgsPack<Args...>;
-// 			
-// 			template <std::size_t Index>
-// 			using Argument = std::tuple_element_t<
-// 				Index,
-// 				std::tuple<Args...>
-// 			>;
-//
-// 			static constexpr auto Arity = sizeof...(Args);
-// 		};
-//
-// 		template <typename ClassType, typename R, typename... Args>
-// 		struct TMemberTraits<R(ClassType::*)(Args...) const &>
-// 		{
-// 			using ReturnType = R;
-// 			using ArgsTuple = TTuple<Args...>;
-// 			using ArgsPack = TArgsPack<Args...>;
-// 			
-// 			template <std::size_t Index>
-// 			using Argument = std::tuple_element_t<
-// 				Index,
-// 				std::tuple<Args...>
-// 			>;
-//
-// 			static constexpr auto Arity = sizeof...(Args);
-// 		};
-//
-// 		template <typename ClassType, typename R, typename... Args>
-// 		struct TMemberTraits<R(ClassType::*)(Args...) const &&>
-// 		{
-// 			using ReturnType = R;
-// 			using ArgsTuple = TTuple<Args...>;
-// 			using ArgsPack = TArgsPack<Args...>;
-// 			
-// 			template <std::size_t Index>
-// 			using Argument = std::tuple_element_t<
-// 				Index,
-// 				std::tuple<Args...>
-// 			>;
-//
-// 			static constexpr auto Arity = sizeof...(Args);
-// 		};
-// #pragma endregion
-//
-// template<typename... Types>
-// struct TKnownTypeList
-// {
-// private:
-// 	template<typename T, typename F>
-// 	static void TryDispatch(FInstancedStruct UntypedStruct, F Functor)
-// 	{
-// 		using Traits = TMemberTraits<std::decay_t<F>>;
-// 		using ArgsTuple = typename Traits::ArgsTuple;
-// 		using FirstArgumentType = std::tuple_element_t<0, ArgsTuple>;
-// 		
-// 		if (T::StaticStruct() == FirstArgumentType::StaticStruct()
-// 			&& T::StaticStruct() == UntypedStruct.GetScriptStruct())
-// 		{
-// 			Functor(UntypedStruct.Get<T>());
-// 		}
-// 	}
-// public:
-// 	template<typename F>
-// 	static void DispatchFor(FInstancedStruct UntypedStruct, F Functor)
-// 	{
-// 		(TryDispatch<Types>(UntypedStruct, Functor), ...);
-// 	}
-// };
+#pragma region TRAITS
+		template<typename...>
+		struct TArgsPack {};
+#pragma endregion
+
+template<typename... Types>
+struct TKnownTypeList
+{
+private:
+	template<typename T, typename F>
+	static void TryDispatch(FInstancedStruct UntypedStruct, F Functor)
+	{
+		if (T::StaticStruct() == UntypedStruct.GetScriptStruct())
+		{
+			Functor(UntypedStruct.Get<T>());
+		}
+	}
+public:
+	template<typename F>
+	static void DispatchFor(FInstancedStruct UntypedStruct, F Functor)
+	{
+		(TryDispatch<Types>(UntypedStruct, Functor), ...);
+	}
+};
 
 
 UCLASS()
